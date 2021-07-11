811ceafe071533cac28c8a238c320fb88ea63a54
SadiaSookram <sadiamsookram@gmail.com>
2021-07-10 23:49:13 -0400

    add ability to ass task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.dir(window.document);
+//Assign button element object rep to var in file
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+ // Adding new text to Task List using DOM > JS
+
+ var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl)};
+
+  buttonEl.addEventListener("click", createTaskHandler);
+
+
+  // 
