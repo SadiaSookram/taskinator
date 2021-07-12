a8a3688ef473fdb950533b2b435ca2d59dcb798c
SadiaSookram <sadiamsookram@gmail.com>
2021-07-11 21:43:42 -0400

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,21 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  
+  // check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+  alert("You need to fill out the task form!");
+  return false;
+}
+formEl.reset(); 
   // package up data as an object
   var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput
   };
-
+  
   // send it as an argument to createTaskEl
-  createTaskEl(taskDataObj);
+  createTaskEl(taskDataObj); 
 };
   
 // Create new task element
