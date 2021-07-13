33c0b0b917fe6984098287670a48da5fc4d53258
SadiaSookram <sadiamsookram@gmail.com>
2021-07-12 21:26:32 -0400

    Task edit and delete buttons added

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ listItemEl.setAttribute("data-task-id", taskIdCounter);
  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
  listItemEl.appendChild(taskInfoEl);
 
+ var taskActionsEl = createTaskActions(taskIdCounter);
+ listItemEl.appendChild(taskActionsEl);
 // add entire list item to list
  tasksToDoEl.appendChild(listItemEl);
  // increase task counter for next unique id
